2203dc86acf8d98564e280f037594aafc8f178db
Talha Enes İslamoglu <talha.enes.1996@gmail.com>
2026-08-11 22:18:59 +0300

    chore: setup initial 3D project scene

---
+++ Assets/Scripts/Player/View/PlayerView.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace FireLine.Scripts.Player.View
+{
+    public class PlayerView : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
