243283257c9933fd9d1492ad0283f4a9fb79bd78
Talha Enes İslamoglu <talha.enes.1996@gmail.com>
2026-08-11 22:45:18 +0300

    feat: implement player MVC movement

--- Assets/Scripts/Player/Controller/PlayerController.cs
+++ Assets/Scripts/Player/Controller/PlayerController.cs
@@ -1,18 +1,72 @@
+using FireLine.Scripts.Player.View;
 using UnityEngine;
+
 namespace FireLine.Scripts.Player.Controller
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [SerializeField] private PlayerView view;
 
+        private PlayerModel model;
+        private Rigidbody rb;
+        private PlayerInputActions inputActions;
+
+        private Vector2 moveInput;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            model = new PlayerModel(
+                moveSpeed: 5f,
+                maxHealth: 100
+            );
+
+            inputActions = new PlayerInputActions();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnEnable()
         {
+            inputActions.Player.Enable();
 
+            inputActions.Player.Move.performed += OnMove;
+            inputActions.Player.Move.canceled += OnMove;
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Player.Move.performed -= OnMove;
+            inputActions.Player.Move.canceled -= OnMove;
+
+            inputActions.Player.Disable();
+        }
+
+        private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            Vector3 direction = new Vector3(
+                moveInput.x,
+                0f,
+                moveInput.y
+            );
+
+            Vector3 velocity = direction * model.MoveSpeed;
+
+            rb.linearVelocity = new Vector3(
+                velocity.x,
+                rb.linearVelocity.y,
+                velocity.z
+            );
         }
     }
 
