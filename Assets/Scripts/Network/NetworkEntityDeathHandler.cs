95fd69b6b40b7b0590f4a0219609e3698c399fd6
Talha Enes İslamoglu <talha.enes.1996@gmail.com>
2026-08-23 16:10:35 +0300

    Implement network player health and death

---
+++ Assets/Scripts/Network/NetworkEntityDeathHandler.cs
@@ -0,0 +1,53 @@
+﻿using FireLine.Scripts.Core.Services.Entities;
+using FireLine.Scripts.Core.Signals;
+using Unity.Netcode;
+using UnityEngine;
+using Zenject;
+
+namespace FireLine.Scripts.Network
+{
+    public class NetworkEntityDeathHandler : MonoBehaviour, IInitializable
+    {
+        private readonly SignalBus _signalBus;
+
+        public NetworkEntityDeathHandler(
+            SignalBus signalBus)
+        {
+            _signalBus = signalBus;
+        }
+
+        public void Initialize()
+        {
+            _signalBus.Subscribe<EntityDestroyedSignal>(
+                OnEntityDestroyed
+            );
+
+            Debug.Log(
+                "[NETWORK DEATH] Subscribed."
+            );
+        }
+
+        private void OnEntityDestroyed(
+            EntityDestroyedSignal signal)
+        {
+            if (signal.Entity is not Entity entity)
+                return;
+
+            NetworkObject networkObject =
+                entity.GetComponent<NetworkObject>();
+
+            if (networkObject == null)
+                return;
+
+            if (!networkObject.IsSpawned)
+                return;
+
+            Debug.Log(
+                $"[NETWORK DEATH] " +
+                $"Despawning: {entity.name}"
+            );
+
+            networkObject.Despawn();
+        }
+    }
+}
