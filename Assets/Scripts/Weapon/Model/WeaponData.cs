c8ca2dbf37d27b228ced41f649c8105e3a4f6684
Talha Enes İslamoglu <talha.enes.1996@gmail.com>
2026-09-07 23:27:23 +0300

    Add 3D positional weapon fire audio

--- Assets/Scripts/Weapon/Model/WeaponData.cs
+++ Assets/Scripts/Weapon/Model/WeaponData.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using FireLine.Scripts.Weapon.Model;
 
 namespace FireLine.Scripts.Weapon.Model
 {
@@ -9,13 +8,19 @@ namespace FireLine.Scripts.Weapon.Model
     )]
     public class WeaponData : ScriptableObject
     {
+        [Header("Weapon")]
         [SerializeField]
         private BulletData bulletData;
 
         [SerializeField]
         private float fireRate = 0.25f;
 
+        [Header("Audio")]
+        [SerializeField]
+        private AudioClip[] fireClips;
+
         public BulletData BulletData => bulletData;
         public float FireRate => fireRate;
+        public AudioClip[] FireClips => fireClips;
     }
 }
---
+++ Assets/Scripts/Weapon/Services/WeaponAudioService.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace FireLine.Scripts.Weapon.Service
+{
+    public class WeaponAudioService
+    {
+        private readonly AudioClip[] _fireClips;
+
+        public WeaponAudioService(
+            AudioClip[] fireClips)
+        {
+            _fireClips = fireClips;
+        }
+
+        public void PlayFireSound(
+            Vector3 position)
+        {
+            if (_fireClips == null ||
+                _fireClips.Length == 0)
+            {
+                Debug.LogWarning(
+                    "[WEAPON-AUDIO] " +
+                    "No fire clips assigned!"
+                );
+
+                return;
+            }
+
+            AudioClip clip =
+                _fireClips[
+                    Random.Range(0, _fireClips.Length)
+                ];
+
+            AudioSource.PlayClipAtPoint(
+                clip,
+                position
+            );
+        }
+    }
+}
